8cb9f03e8b707470c33ebe670c94302898fa6a7c
Majidshah83 <shahmajid508@gmail.com>
2022-11-18 00:54:47 +0500

    Retriviving single and multiple row/column

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -8,8 +8,13 @@ class StudentController extends Controller
 {
     public function getstudent()
     {
-        //Retriviving All Rows From table 
+        //Retriviving All Rows From table
         $result=DB::table('student')->get();
+        //Retriviving single and multiple row/column from table
+        $result=DB::table('student')->where('city','mardan')->get();
+        $result=DB::table('student')->where('id','>',2)->get();
+        $result=DB::table('student')->where('id','>',2)->first();
+        $result=DB::table('student')->where('marks','>',370)->get();
         return $result;
      }
 }
