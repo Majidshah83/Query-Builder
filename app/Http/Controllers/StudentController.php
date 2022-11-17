d94e4068b282051016606d8df91be3662a7e6c0f
Majidshah83 <shahmajid508@gmail.com>
2022-11-18 01:03:25 +0500

    use pluck and value

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -10,11 +10,14 @@ class StudentController extends Controller
     {
         //Retriviving All Rows From table
         $result=DB::table('student')->get();
-        //Retriviving single and multiple row/column from table
+        // Retriviving single and multiple row/column from table
         $result=DB::table('student')->where('city','mardan')->get();
         $result=DB::table('student')->where('id','>',2)->get();
         $result=DB::table('student')->where('id','>',2)->first();
         $result=DB::table('student')->where('marks','>',370)->get();
+        $result=DB::table('student')->where('marks','>',370)->pluck('name');
+        $result=DB::table('student')->where('marks','>',370)->value('name');
+
         return $result;
      }
 }
